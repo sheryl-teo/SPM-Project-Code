c0c93dd7b843969a2512443f99189b6dfaf67e57
shiroscape <105692514+sheryl-teo@users.noreply.github.com>
2022-09-28 11:20:00 +0800

    components created

---
+++ spm/src/components/staff-card/staff-card.styles.jsx
@@ -0,0 +1,22 @@
+
+const staffCard = ({}) => {
+
+    const {id, name, email} = staff;
+
+    return (
+        <div className="card-container">
+
+                <img src = {}/>
+                <h2>{}</h2>
+                <p>{}</p>
+        </div>
+
+    )
+
+
+
+}
+
+
+
+export default staffCard;
