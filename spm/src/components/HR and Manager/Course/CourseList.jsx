abd2414252c99c31887dbc8b2b81b3506130427c
Zuyao <zuyao.liu.2020@business.smu.edu.sg>
2022-10-08 22:15:14 +0800

    Course Components & Route
    - Added new components for courses
    - Added routing

---
+++ spm/src/components/HR and Manager/Course/CourseList.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+function CourseList(props) {
+    // let courses = props.courses
+
+    // const CourseListItems = courses.map(
+    //     (course) => {
+    //         return (
+    //             <li>{course.Course_ID}: {course.Course_Name}</li>
+    //         )
+    //     }
+    // )
+    return (
+        <div>
+            <h2>Course List</h2>
+            <ol>
+                <div>Courses Available:
+
+                </div>
+            </ol>
+        </div>
+    )
+}
+
+export default CourseList;
---
+++ spm/src/components/HR and Manager/HR_home.jsx
@@ -0,0 +1,29 @@
+
+import React from 'react';
+
+
+function HR () {
+  return (
+    <div>
+      <h1 className='header'>
+        Skills
+      </h1>
+
+      <h1 className='header'>
+        Courses
+      </h1>
+
+      <h1 className='header'>
+        Roles
+      </h1>
+      
+      
+
+
+
+
+    </div>
+  );
+};
+
+export default HR;
