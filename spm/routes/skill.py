4f47d432aeeea9a91e7568db351c949b1f17cb6c
patmloi <patriciamloihy@gmail.com>
2022-10-25 14:25:13 +0800

    Update skill.py
    Skill read functions: Filter by skill ID, name

--- spm/routes/skill.py
+++ spm/routes/skill.py
@@ -16,6 +16,45 @@ skill = APIRouter()
 def get_all_skill():
     return conn.execute(skills.select()).fetchall()
 
+# Read 
+@skill.get(
+    "/skills/read/id/{search_skill_ID}",
+    tags=["skills"],
+    response_model=List[Skill],
+    description="Reads a skill through an exact match search by skill ID.",
+)
+def read_skill_id(search_skill_ID: str):
+    """Reads a skill through an exact match search by skill ID.
+
+    Args:
+        search_skill_ID (str): Skill ID, following the format "SXX", 
+        with XX representing the skill number.
+
+    Returns:
+        _type_: _description_
+    """
+    search_skill_ID = search_skill_ID.upper()
+    statement = skills.select().where(skills.c.Skill_ID==search_skill_ID)
+    return conn.execute(statement).all()
+
+@skill.get(
+    "/skills/read/name/{search_skill_name}",
+    tags=["skills"],
+    response_model=List[Skill],
+    description="Reads a skill through a substring match search by skill name.",
+)
+def read_skill_name(search_skill_name: str):
+    """Reads a skill through a substring match search by skill name.
+
+    Args:
+        search_skill_name (str): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    search_skill_name = search_skill_name.lower()
+    statement = skills.select().where(func.lower(skills.c.Skill_Name).contains(search_skill_name))
+    return conn.execute(statement).all()
 
 # @skill.put(
 #     "/delete_skill_course/{Skill_ID}{Course_ID}",
