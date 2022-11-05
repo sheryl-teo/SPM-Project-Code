900f562a842451f37b4443ac4d0ff3e24f260a93
zy448 <92178505+zy448@users.noreply.github.com>
2022-11-05 19:31:52 +0800

    creation save details

--- spm/src/App.js
+++ spm/src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route}
 import Home from './components/home';
 import Profile from './components/Staff/profile/profile';
 import Staff from './components/Staff/staff_home';
+import Details from './components/Staff/details/details';
 import HR from './components/HR and Manager/HR_home'
 import CourseList from './components/HR and Manager/Course/CourseList'
 import CourseInfo from './components/HR and Manager/Course/CourseInfo'
@@ -14,50 +15,50 @@ import LJCreationPage from './components/Staff/LJCreationPage/LJ_Creation_Page';
 function App() {
 
 	// Test Data 
-	const courses = [ 
-		{
-		  Course_ID: "COR001",
-		  Course_Name: "Systems Thinking and Design",
-		  Course_Desc: "This foundation module aims to introduce students to the fundamental concepts and underlying principles of systems thinking",
-		  Course_Status: "Active",
-		  Course_Type: "Internal",
-		  Course_Category: "Core",
-		},
+	// const courses = [ 
+	// 	{
+	// 	  Course_ID: "COR001",
+	// 	  Course_Name: "Systems Thinking and Design",
+	// 	  Course_Desc: "This foundation module aims to introduce students to the fundamental concepts and underlying principles of systems thinking",
+	// 	  Course_Status: "Active",
+	// 	  Course_Type: "Internal",
+	// 	  Course_Category: "Core",
+	// 	},
 	
-		{
-		  Course_ID:"COR002",
-		  Course_Name: "Lean Six Sigma Green Belt Certification",
-		  Course_Desc: "Apply Lean Six Sigma methodology and statistical tools such as Minitab to be used in process analytics",
-		  Course_Status: "Active",
-		  Course_Type: "Internal",
-		  Course_Category: "Core",
-		},
+	// 	{
+	// 	  Course_ID:"COR002",
+	// 	  Course_Name: "Lean Six Sigma Green Belt Certification",
+	// 	  Course_Desc: "Apply Lean Six Sigma methodology and statistical tools such as Minitab to be used in process analytics",
+	// 	  Course_Status: "Active",
+	// 	  Course_Type: "Internal",
+	// 	  Course_Category: "Core",
+	// 	},
 	
-		{
-		  Course_ID:"COR004",
-		  Course_Name: "Service Excellence",
-		  Course_Desc: "The programme provides the learner with the key foundations of what builds customer confidence in the service industry",
-		  Course_Status: "Pending",
-		  Course_Type: "Internal",
-		  Course_Category: "Core",
-		}
-	  ] 
-	  ;
+	// 	{
+	// 	  Course_ID:"COR004",
+	// 	  Course_Name: "Service Excellence",
+	// 	  Course_Desc: "The programme provides the learner with the key foundations of what builds customer confidence in the service industry",
+	// 	  Course_Status: "Pending",
+	// 	  Course_Type: "Internal",
+	// 	  Course_Category: "Core",
+	// 	}
+	//   ] 
+	//   ;
 
-		const CourseRoutes = courses.map(
+	// 	const CourseRoutes = courses.map(
 
-			(course) => {
-				console.log(course)
+	// 		(course) => {
+	// 			console.log(course)
 				
-				return (
-					<>
-					<Route path = {`/HR/Courses/${course.Course_ID}`} element = {CourseInfo(course)}/>
-					<Route path = {`/HR/Courses/${course.Course_ID}/CourseAdd`} element = {CourseAdd(course)}/>
-					</>
+	// 			return (
+	// 				<>
+	// 				<Route path = {`/HR/Courses/${course.Course_ID}`} element = {CourseInfo(course)}/>
+	// 				<Route path = {`/HR/Courses/${course.Course_ID}/CourseAdd`} element = {CourseAdd(course)}/>
+	// 				</>
 					
-				)
-			}
-		)
+	// 			)
+	// 		}
+	// 	)
 	  
 	
 return (
@@ -67,9 +68,10 @@ return (
 		<Route exact path='/' element={<Home />} />
 		<Route path="/staff" element={<Staff />} />
 		<Route path="/staff/learningjourneyCreation" element={<LJCreationPage />} />
+		<Route path="/staff/learningjourneyCreation/details" element={<Details />} />
 		<Route path = "/HR" element = {<HR />}/>
-		<Route path = "/HR/Courses" element = {CourseList(courses)}/>
-		{CourseRoutes}
+		{/* <Route path = "/HR/Courses" element = {CourseList(courses)}/>
+		{CourseRoutes} */}
 
 
 		
--- spm/src/components/Staff/LJCreationPage/LJ_Creation_Page.jsx
+++ spm/src/components/Staff/LJCreationPage/LJ_Creation_Page.jsx
@@ -2,6 +2,8 @@
 import React from 'react';
 import "./LJ_Creation_Page.css"
 
+
+
 //remove present options for skill list
 function removeOptions(selectElement) {
   var i, L = selectElement.options.length - 1;
@@ -14,12 +16,12 @@ function removeOptions(selectElement) {
 const LJCreationPage = () => {
   var coursesArray = []
   var skillsArray = []
-  var courseList = document.getElementById('courses');
-  var skillList = document.getElementById('skills');
+
 
   //alternate skill droplist according to job role
   function displayCourses(event) {
-    
+    var courseList = document.getElementById('courses');
+
     var skill = event.target.value; //job role selection dynamic value
 
     //assigning diff job position diff skill-list
@@ -28,10 +30,10 @@ const LJCreationPage = () => {
 
 
     if (skill == "admin first skill") {
-      coursesArray = [starter,'admin first course', 'admin second course', 'admin third course'];
+      coursesArray = [starter, 'admin first course', 'admin second course', 'admin third course'];
     }
     else if (skill == 'manager first skill') {
-      coursesArray = [starter,'manager first course', 'manager second course', 'manager third course']
+      coursesArray = [starter, 'manager first course', 'manager second course', 'manager third course']
     }
     else {
       coursesArray = [noCourse]
@@ -53,6 +55,9 @@ const LJCreationPage = () => {
 
 
   function displaySkills(jobPosition) {
+    var skillList = document.getElementById('skills');
+    var courseList = document.getElementById('courses');
+
     console.log(" skills function working");
     var job = jobPosition.target.value; //job role selection dynamic value
 
@@ -61,14 +66,14 @@ const LJCreationPage = () => {
     var noSkill = "No Skill Available";
 
     if (job == "admin") {
-      skillsArray = [starter,'admin first skill', 'admin second skill', 'admin third skill']
+      skillsArray = [starter, 'admin first skill', 'admin second skill', 'admin third skill']
     }
     else if (job == 'manager') {
-      skillsArray = [starter,'manager first skill', 'manager second skill', 'manager third skill']
+      skillsArray = [starter, 'manager first skill', 'manager second skill', 'manager third skill']
     }
     else {
       skillsArray = [noSkill]
-      
+
     }
 
     if (skillList.options.length != 0) {
@@ -82,11 +87,28 @@ const LJCreationPage = () => {
       newoption.innerHTML = skillsArray[skill]
       skillList.options.add(newoption)
     }
-    
+
     if (courseList.options.length != 0) {
       removeOptions(courseList);
     }
   };
+
+
+  function handleSubmit(event) {
+    
+    const formData = new FormData(event.currentTarget);
+    event.preventDefault();  // prevent page refresh
+    for (let [key, value] of formData.entries()) {
+      console.log(key, value); // push to backend
+    };
+  
+  };
+
+  function poppin() {
+    alert("Learning Journey Created!");
+    window.location.replace("/staff");
+  }
+
 
   //html
   return (
@@ -94,36 +116,93 @@ const LJCreationPage = () => {
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css"
         integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4"
         crossorigin="anonymous"></link>
-      <form action="" method='post'>
-        <h1>Registration Form</h1>
-        <p>
-          <input type="text" className="name" class="formStyle" placeholder="Name (required)" required />
-        </p>
-        <p>
-          <input type="text" className="id" class="formStyle" placeholder="Staff_ID (required)" required />
-        </p>
-        <p>
 
+
+
+
+      <form onSubmit={handleSubmit}>
+        <h1>Registration Form</h1>
+        {/* <input type="text" name="username" placeholder="Email" />
+        <input type="password" name="password" placeholder="Password" /> */}
+        <p>
+          <input type="text" className="name" name="name" class="formStyle" placeholder="Name (required)" required />
         </p>
         <p>
-          <select class="jobSkills" data-size="4" onChange={displaySkills} id="job" required>
+          <input type="text" className="id" name="id" class="formStyle" placeholder="Staff_ID (required)" required />
+        </p>
+
+        <p>
+          <select className="jobSkills" name="job" data-size="4" onChange={displaySkills} id="job" required>
             <option value="">Select Job Position</option>
             <option value="admin">Admin</option>
             <option value="manager">Manager</option>
           </select>
         </p>
         <p>
-          <select class="jobSkills" data-size="4" onChange={displayCourses} id="skills" required>
+          <select className="jobSkills" name="skills" data-size="4" onChange={displayCourses} id="skills" required>
           </select>
         </p>
         <p>
-          <select class="jobSkills" data-size="4" id='courses'>
+          <select className="jobSkills" name='courses' data-size="4" id='courses'>
           </select>
         </p>
-        <button type="submit" class="btn btn-light">Submit</button>
+
+
+        <button type="submit" class="btn btn-dark" onClick={poppin}>Submit</button>
       </form>
+
+
+
+
     </div>
   );
 };
 
 export default LJCreationPage;
+
+
+
+//working -------
+
+// const handleSubmit = (event) => {
+//   const formData = new FormData(event.currentTarget);
+//   event.preventDefault();
+//   for (let [key, value] of formData.entries()) {
+//     console.log(key, value);
+//   }
+// };
+
+// <form onSubmit={handleSubmit}>
+// <input type="text" name="username" placeholder="Email" />
+// <input type="password" name="password" placeholder="Password" />
+// <button type="submit">Login</button>
+// </form>
+
+
+
+
+// original -------
+{/* <h1>Registration Form</h1>
+<p>
+  <input type="text" className="name" class="formStyle" placeholder="Name (required)" required />
+</p>
+<p>
+  <input type="text" className="id" class="formStyle" placeholder="Staff_ID (required)" required />
+</p>
+
+<p>
+  <select class="jobSkills" data-size="4" onChange={displaySkills} id="job" required>
+    <option value="">Select Job Position</option>
+    <option value="admin">Admin</option>
+    <option value="manager">Manager</option>
+  </select>
+</p>
+<p>
+  <select class="jobSkills" data-size="4" onChange={displayCourses} id="skills" required>
+  </select>
+</p>
+<p>
+  <select class="jobSkills" data-size="4" id='courses'>
+  </select>
+</p>
+<button type="submit" class="btn btn-dark">Submit</button> */}
