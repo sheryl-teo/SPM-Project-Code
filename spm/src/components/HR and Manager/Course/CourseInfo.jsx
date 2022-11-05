49e449a818e800fba5922e72ab924771d7bfb53a
Zuyao <zuyao.liu.2020@business.smu.edu.sg>
2022-11-05 17:15:29 +0800

    Integration with Backend for Skills and Courses

--- spm/src/components/HR and Manager/Course/CourseInfo.jsx
+++ spm/src/components/HR and Manager/Course/CourseInfo.jsx
@@ -1,48 +1,49 @@
 import {BrowserRouter as Router, Link} from 'react-router-dom';
 
 function CourseInfo(props) {
-    console.log(props)
+    
     return (
+        <h1>hello</h1>
 
-        <div className='expense-item__description'>
-            <h2>{props.Course_ID}</h2>
-            <div className='expense-item__price'>Course Name: <br />{props.Course_Name}</div>
-            <br />
-            <div className='expense-item__price'>Course Description: {props.Course_Desc}</div>
-            <br />
-            <div className='expense-item__price'>Course Status: {props.Course_Status}</div>
-            <br />
-            <div className='expense-item__price'>Course Type: {props.Course_Type}</div>
-            <br />
-            <div className='expense-item__price'>Course Category: {props.Course_Category}</div>
-            <br />
+        // <div className='expense-item__description'>
+        //     <h2>{props.Course_ID}</h2>
+        //     <div className='expense-item__price'>Course Name: <br />{props.Course_Name}</div>
+        //     <br />
+        //     <div className='expense-item__price'>Course Description: {props.Course_Desc}</div>
+        //     <br />
+        //     <div className='expense-item__price'>Course Status: {props.Course_Status}</div>
+        //     <br />
+        //     <div className='expense-item__price'>Course Type: {props.Course_Type}</div>
+        //     <br />
+        //     <div className='expense-item__price'>Course Category: {props.Course_Category}</div>
+        //     <br />
 
             
-            <Link to="/Hr/courses">
-                <button type="button">
-                    Back
-                </button>
-            </Link>
+        //     <Link to="/Hr/courses">
+        //         <button type="button">
+        //             Back
+        //         </button>
+        //     </Link>
 
-            <Link to="">
-                <button type="button">
-                    Edit Courses
-                </button>
-            </Link>
+        //     <Link to="">
+        //         <button type="button">
+        //             Edit Courses
+        //         </button>
+        //     </Link>
         
-            <Link to={`CourseAdd`}>
-                <button type="button">
-                    Add Courses
-                </button>
-            </Link>
+        //     <Link to={`CourseAdd`}>
+        //         <button type="button">
+        //             Add Courses
+        //         </button>
+        //     </Link>
 
-            <Link to="">
-                <button type="button">
-                    Delete Courses
-                </button>
-            </Link>
+        //     <Link to="">
+        //         <button type="button">
+        //             Delete Courses
+        //         </button>
+        //     </Link>
 
-        </div>
+        // </div>
 
 
     )
